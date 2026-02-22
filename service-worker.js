c63aa9f5b8cbba355140699f087d33d7b8858e1e
LM-Life <baker8806@live.com>
2026-02-22 23:44:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Loadmaster Pro service-worker.js
 // NOTE: Bump CACHE_NAME every time you want users to get a fresh cache.
-const CACHE_NAME = "v2026.02.22-db1d5cc";
+const CACHE_NAME = "v2026.02.23-db1d5cc";
 
 const CORE_FILES = [
   "./",

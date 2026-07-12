64844bdc95d567ff6a5d8a08a5652cf4263015b8
LM-Life <baker8806@live.com>
2026-07-12 17:12:32 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Loadmaster Pro service-worker.js
 // NOTE: Bump CACHE_NAME every time you want users to get a fresh cache.
-const CACHE_NAME = "v2026.07.12-020dcb5";
+const CACHE_NAME = "v2026.07.12-pmcr";
 
 const CORE_FILES = [
   "./",
